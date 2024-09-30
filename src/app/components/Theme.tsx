53ddc883766b9961e594b5dc79c306d71d9e7cd4
Devduttsinh <devduttchudasama1339@gmail.com>
2024-10-01 01:46:25 +0530

    basic SEO ready now can replace

--- src/app/components/Theme.tsx
+++ src/app/components/Theme.tsx
@@ -10,8 +10,6 @@ export default function Theme() {
       paraText: '139 5 102',
       highlight: '127 90 240',
       orangeColor: '200 146 11',
-      // cardBack: '47 50 55',
-      // cursorColor: '0 116 44',
     },
     {
       background: '0 0 0',
@@ -19,8 +17,6 @@ export default function Theme() {
       paraText: '193 21 21',
       highlight: '127 90 240',
       orangeColor: '233 207 39',
-      // cardBack: '0 0 0',
-      // cursorColor: '0 113 44',
     },
     {
       background: '47 47 47',
@@ -28,8 +24,6 @@ export default function Theme() {
       paraText: '3 255 82',
       highlight: '111 66 245',
       orangeColor: '204 147 3',
-      // cardBack: '61 56 56',
-      // cursorColor: '254 212 0',
     },
     {
       background: '45 8 34',
@@ -37,8 +31,6 @@ export default function Theme() {
       paraText: '148 161 178',
       highlight: '3 255 82',
       orangeColor: '2 154 1',
-      // cardBack: '63 33 54',
-      // cursorColor: '2 154 1',
     },
     {
       background: '13 14 19',
@@ -46,8 +38,14 @@ export default function Theme() {
       paraText: '148 161 178',
       highlight: '111 66 245',
       orangeColor: '0 221 255',
-      // cardBack: '21 22 26',
-      // cursorColor: '2 154 1',
+    },
+    {
+      background: '0 0 0',
+      foreground: '208 208 208',
+      highlight: '255 101 0',
+      link: '67 121 242',
+      orangeColor: '199 152 33',
+      shade: '31 31 31',
     },
   ];
   const [theme, setTheme] = useState((history.length + 1) % themes.length);
